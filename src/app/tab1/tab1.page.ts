4b0e4b1dc401099cc795d7d7e6eb60a8b519c826
Jorge <jambarreto@gmail.com>
2020-03-05 17:56:25 -0300

    Alteracoes

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,6 +1,5 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
@@ -8,16 +7,15 @@ import { Router } from '@angular/router';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+   
   constructor(public alertController: AlertController) {
     
   }
   @Input() vHora1;   
   @Input() vBruto;
-  @Output() resultado = new EventEmitter();
   
   async showHora() {
-    var valorhora = (((this.vBruto*6) - 28)/(this.vHora1 * 31.5)).toFixed(2);
-    
+    const valorhora = (((this.vBruto*6) - 28)/(this.vHora1 * 31.5)).toFixed(2);
     const alert = await this.alertController.create({
       header: 'Valor da hora aula',
       //subHeader: 'Valor da hora aula',
@@ -28,6 +26,5 @@ export class Tab1Page {
     await alert.present();
     this.vHora1=""
     this.vBruto=""
-  }
-  
+  }  
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,5 @@
 import { Component, Input, Output } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-import { ActivatedRoute } from '@angular/router';
-
 
 @Component({
   selector: 'app-tab2',
@@ -10,26 +8,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Tab2Page {
 
-  constructor(public alertController: AlertController) {
-  }
+  constructor(public alertController: AlertController) { }
 
   @Input() vHora;
-  @Output() vHora1;
   @Input() nHora;
-
+     
   async showSalario() {
     var salario = ((31.5 * this.vHora * this.nHora + 28)/6).toFixed(2);
-
     const alert = await this.alertController.create({
       header: 'Valor do salário',
-      //subHeader: 'Valor da hora aula',
       message: salario, 
       buttons: ['OK']
     });
-  
-    await alert.present();
     this.vHora=""
     this.nHora=""
+    await alert.present();
+    
   }
 
 }
